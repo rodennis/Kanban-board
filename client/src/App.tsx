13ed9457364cc47efb546fadc9c7adfb84ff85e2
rodennis <rev.rltodd@gmail.com>
2022-04-20 22:14:42 -0500

    setup post and get request functions for firebase db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import './App.css';
+import { CreateList } from './components/createList/CreateList';
+import './firebase/firebase'
+import realtime from './firebase/realtime';
 
-function App() {
+interface listItems {
+  key: string
+  name:any
+}
+
+const App: FC = () => {
+
+
+  const [listName, setListName] = useState<string>('')
+  const [lists, setLists] = useState<listItems[]>([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await realtime.get('/lists.json')
+      const data = res?.data
+      if (data !== null) {
+        const returnedLists = Object.entries(data).map(
+          ([key, name]) => ({
+            key,
+            name
+          })
+        );
+        setLists(returnedLists);
+      }
+    }
+    getData()
+  });
+  
   return (
     <div className="App">
-      
+      <CreateList setListName={setListName} listName={listName}/>
+      {
+        lists.map(item => (
+          <div>
+            <h1>{item.name.name}</h1>
+          </div>
+        ))
+      }
     </div>
   );
 }
---
+++ client/src/components/createList/CreateList.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react'
+import realtime from '../../firebase/realtime'
+
+interface handleSubmit {
+    setListName: (event: any) => void
+    listName: string
+  }
+
+export const CreateList:FC<handleSubmit> = ({ setListName, listName}) => {
+
+    class List {
+        list: any;
+        name: string;
+        constructor(name: string) {
+          this.list = []
+          this.name = name
+        }
+    
+        // addItem(val:string, ){
+    
+        // }
+      }
+
+      const handleSubmit = async (event: any) => {
+        event.preventDefault()
+        const newList = new List(listName)
+        git addawait realtime.post('/lists.json', newList)
+      }
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+      <input type="text" onChange={event => setListName(event.target.value)}/>
+      <button>Submit</button>
+      </form>
+    </div>
+  )
+}
